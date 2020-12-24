dc2ea904c71ae927c331b12ee8dbcdf0239e6ef0
chintan mehta <chintan.mehta@SW-LP07198.local>
2020-12-24 12:38:11 +0530

    test

---
+++ src/main/java/JenkinsWorld.java
@@ -0,0 +1,7 @@
+public class JenkinsWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!!");
+    }
+}
---
+++ src/test/java/TestJenkins.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class TestJenkins {
+
+    @Test
+    public void test(){
+        System.out.println("TestJenkins!!");
+    }
+}
